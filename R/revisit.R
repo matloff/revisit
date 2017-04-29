240f993b299678a6c28df56e6118dd72a66ed25a
Norm Matloff <matloff@cs.ucdavis.edu>
2017-04-28 23:37:52 -0700

    have working code!

--- R/revisit.R
+++ R/revisit.R
@@ -1,22 +1,55 @@
 
-# initialize rvisit
+# current code restrictions:
 
+#    loops, if(), if() else must be contained in a single line
+#    (semicolons allowed)
+
+#    a pause should be placed after each action that produces a display,
+#    e.g. hist(), by inserting a line like
+
+#       readline('hit Enter ')
+
+# initialize rvisit
 rvinit <- function() {
-   rvenv <- new.env()
+   rvenv <<- new.env()
    rvenv$currb <- NULL
    rvenv$currcode <- NULL
+   rvenv$pc <- NULL
+}
+
+# load original code and make the first branch from it
+makebranch0 <- function(origcodenm) {
+   code <- readLines(con = origcodenm)
+   attr(code,'desc') <- 'original'
+   save(code,file='branch0')
+}
+
+# create new branch, with file name br; saves rvenv$currcode;
+# optional note desc describes the branch;
+saveb <- function(br,desc=NULL) {
+   code <- rvenv$currcode
+   attr(code,'desc') <- desc
+   save(code,file=br)
 }
 
 # set current branch to br, a filename
-setb <- function(br) {
+loadb <- function(br) {
    rvenv$currb <- br
-   rvenv$currcode <- readLines(con=br)
+   load(br)  # variable 'code' will now exist
+   rvenv$currcode <- code
+   rvenv$pc <- 1  # see note above on loops etc.
 }
 
-runb <- function(throughline=length((rvenv$currcode)) {
-   for (codeline in rvenv$currcode) {
-      docmd(codeline)
-   }
+runb <- function(
+           startline = rvenv$pc,
+           throughline=length(rvenv$currcode))  {
+        if (startline < 1 || throughline > length(rvenv$currcode))
+           stop('line number out of range')
+        execrange <- startline:throughline
+        for (codeline in execrange) {
+           docmd(rvenv$currcode[codeline])
+        }
+        rvenv$pc <- throughline + 1
 }
 
 # do one line of code from a branch
