e9ba734bc8a607024a87de51859e9e48c095278a
matloff <matloff@cs.ucdavis.edu>
2017-05-05 16:58:52 -0700

    cleanup

--- R/pima.0.R
+++
@@ -1,4 +0,0 @@
-# original code
-pima <- read.csv('http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data',header=FALSE)
-colnames(pima) <- c('NPreg','Gluc','BP','Thick','Insul','BMI','Genet','Age','Diab')
-print(summary(glm(Diab ~ .,data=pima)))
--- R/z.R
+++
@@ -1,4 +0,0 @@
-x <- runif(100)
-y <- runif(100)
-z <- x + y
-hist(z)
