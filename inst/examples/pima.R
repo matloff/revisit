fc44f6cc794feca1a8d7c83243ba2a047d287cbe
matloff <matloff@cs.ucdavis.edu>
2017-05-05 17:00:50 -0700

    pima.R now calls data(pima)

--- inst/examples/pima.R
+++ inst/examples/pima.R
@@ -1,3 +1,2 @@
-pima <- read.csv('http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data',header=FALSE)
-colnames(pima) <- c('NPreg','Gluc','BP','Thick','Insul','BMI','Genet','Age','Diab')
+data(pima)
 print(summary(glm(Diab ~ .,data=pima)))
