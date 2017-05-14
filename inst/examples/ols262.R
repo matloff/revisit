559aae941e522703a6307939d4c062fda40e2356
Norm Matloff <matloff@cs.ucdavis.edu>
2017-05-13 21:22:18 -0700

    cleanup the code

--- inst/examples/ols262.R
+++ inst/examples/ols262.R
@@ -1,13 +1,22 @@
-# The file public.dta and the Stata code on which this R code is based is courtesy of M. Zavodny
-data(zav) # zav.txt is .txt version of public.dta
-zav = zav[zav$year < 2008,] # 2000-2007 (since first year in zav.txt is 2000)
+# the file public.dta and the Stata code on which this R code is based
+# is courtesy of M. Zavodny
+
+data(zav) # zav.txt is .txt version of Zav. file public.dta
+zav = zav[zav$year < 2008,] # 2000-2007 (first year in zav.txt is 2000)
 
 ##### traditional employment rate and immigrant share model #####
-# employment rate for natives = employed natives divided by total native population
+
+# employment rate for natives = employed natives divided by total native
+# population
 zav$emprate_native   <- zav$emp_native / zav$pop_native * 100
-# immigrant stem workers with advanced degrees from U.S. universities - share of total workers employed
+
+# immigrant stem workers with advanced degrees from U.S. universities, 
+# share of total workers employed
+
 zav$immshare_emp_stem_e_grad   <- zav$emp_edus_stem_grad / zav$emp_total * 100
-# immigrant stem workers with advanced degrees from non-U.S. universities - share of total workers employed
+
+# immigrant stem workers with advanced degrees from non-U.S.
+# universities - share of total workers employed
 zav$immshare_emp_stem_n_grad   <- zav$emp_nedus_stem_grad / zav$emp_total * 100
 
 # logs of prior three values
@@ -15,9 +24,12 @@ zav$lnemprate_native           <- log(zav$emprate_native)
 zav$lnimmshare_emp_stem_e_grad <- log(zav$immshare_emp_stem_e_grad)
 zav$lnimmshare_emp_stem_n_grad <- log(zav$immshare_emp_stem_n_grad)
 
-# Years in sample (in this case, 2000-2007), converted to factors for use as an instrumental variable
+# year in sample (in this case, 2000-2007), converted to factors for
+# use as an instrumental variable
 zav$fyear  <- as.factor(zav$year)
-# 50 states plus Washington D.C., converted to factors for use as an instrumental variable
+
+# 50 states plus Washington D.C., converted to factors for use as an
+# instrumental variable
 zav$fstate <- as.factor(zav$statefip)
 
 # need to normalize population weights so that each year has same weight;
@@ -26,10 +38,20 @@ zav$fstate <- as.factor(zav$statefip)
 zav$sum_pop_native <- with(zav, ave(pop_native, year, FUN=sum))
 zav$weight_native <- zav$pop_native / zav$sum_pop_native
 
-# remove non-positive values so that their logs are valid values (not NaN or infinite, plus or minus)
+# remove non-positive values so that their logs are valid values (not
+# NaN or infinite, plus or minus)
 kk <- zav[zav$emp_edus_stem_grad > 0 & zav$emp_nedus_stem_grad > 0,]
-# do regression with two independent variables, two instrumental variables, and weights to treat years equally
-mm <- (with(kk, lm(lnemprate_native ~ lnimmshare_emp_stem_e_grad + lnimmshare_emp_stem_n_grad + fyear + fstate, weights=weight_native)))
+
+# do regression with two independent variables, two instrumental
+# variables, and weights to treat years equally
+
+mm <- (with(kk, 
+   lm(lnemprate_native ~ 
+      lnimmshare_emp_stem_e_grad + 
+      lnimmshare_emp_stem_n_grad + 
+      fyear + 
+      fstate, 
+      weights=weight_native)))
 
 # extract slope and p-value from summary
 slope <- mm$coefficients[2]
