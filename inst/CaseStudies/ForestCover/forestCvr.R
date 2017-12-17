eae09749e942f8af394ba7292d4381ec66cd4b35
Norm Matloff <matloff@cs.ucdavis.edu>
2017-12-16 21:11:04 -0800

    did more on forest cover example

--- inst/CaseStudies/ForestCover/forestCvr.R
+++ inst/CaseStudies/ForestCover/forestCvr.R
@@ -2,9 +2,13 @@
 # analysis of forest cover type  data, 
 # https://archive.ics.uci.edu/ml/datasets/covertype
 
-if (!file.exists("./data/cover.data")){
-   download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.data.gz",'frst.data')
-   con <- gzfile('frst.data')
-   frst <- readLines(con)
+library(R.utils)
+datapath <- system.file("data", package="revisit")
+filepath <- paste0(datapath, "/cover.data")
+if (!file.exists(filepath)){
+   download.file("https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.data.gz",'cover.data.gz')
+   gunzip('cover.data.gz')
+   file.copy('cover.data',filepath)
+   file.remove('cover.data')
 }
 
