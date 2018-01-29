a636f56c5509af6125f5a031ea3b6b8bf6653626
B Davis <rdavis00027@gmail.com>
2018-01-28 23:45:00 -0800

    Add example that reads GDP excel file from BEA and plots GDP data.

--- R/revisit.R
+++ R/revisit.R
@@ -12,6 +12,8 @@
 # initialize rvisit; the R environment rvenv will contain the relevant
 # information about the currently-in-use branch
 
+library(readxl)
+
 rvinit <- function(smalleffect=0.05) {
    rvenv <<- new.env()
    rvenv$currb <<- NULL
@@ -261,4 +263,45 @@ getexample <- function(exname) {
    fullexname <- paste('CaseStudies/',exname,sep='')
    loc <- system.file(fullexname,package='revisit')
    file.copy(loc,'.')
+}
+
+
+# gets GDP data from the Bureau of Economic Analysis web site;
+# span="year" (default) or "quarter" - get annual or quarterly GDP data;
+# overwrite=FALSE (default) or TRUE  - overwrite local data if already gotten;
+# year fields: year, gdp_curr, gdp_const, change=real growth of year (percent);
+# quarter fields: quarter, gdp_curr, gdp_const, change=real annualized growth of quarter,
+#  change4q=real growth of last 4 quarters; iyear, qtr, & year - calculated from quarter
+
+getGDP <- function(span="year",overwrite=FALSE) {
+   datapath <- system.file("data", package="revisit")
+   filepath <- paste0(datapath, "/gdplev.xlsx")
+   if (!file.exists(filepath) | overwrite == TRUE) {
+      download.file("https://www.bea.gov/national/xls/gdplev.xlsx", "gdplev.xlsx", mode = "wb")
+      file.rename("gdplev.xlsx", filepath)
+   }
+   xx <- read_excel(filepath, col_names = FALSE, skip = 8)
+   if (span == "year") {
+      gg <- as.data.frame(xx[!is.na(xx[,1]),1:3])
+      colnames(gg) <- c("year", "gdp_curr", "gdp_const")
+      change <- 100 * diff(gg$gdp_const, 1) / gg$gdp_const[1:(length(gg$gdp_const)-1)]
+      gg$change <- NA
+      gg$change[2:(length(gg$change))] <- change
+      gg$year <- as.numeric(gg$year)
+   }
+   else {
+      gg <- as.data.frame(xx[!is.na(xx[,5]),5:7])
+      colnames(gg) <- c("quarter", "gdp_curr", "gdp_const")
+      change <- 100 * (diff(gg$gdp_const, 1) / gg$gdp_const[1:(length(gg$gdp_const)-1)] + 1)^4 - 100
+      gg$change <- NA
+      gg$change[2:(length(gg$change))] <- change
+      change4q <- 100 * diff(gg$gdp_const, 4) / gg$gdp_const[1:(length(gg$gdp_const)-4)]
+      gg$change4q <- NA
+      gg$change4q[5:(length(gg$change4q))] <- change4q
+      
+      gg$iyear <- as.numeric(substr(gg$quarter,1,4))
+      gg$qtr   <- as.numeric(substr(gg$quarter,6,6))
+      gg$year  <- gg$iyear + (gg$qtr-1)/4
+   }
+   gdp <- gg
 }
---
+++ inst/CaseStudies/GDP/GDP.R
@@ -0,0 +1,30 @@
+aa <- getGDP(span="year", overwrite="TRUE")
+qq <- getGDP(span="quarter")
+print(head(aa))
+print(tail(aa))
+print(head(qq))
+print(tail(qq))
+
+qq$change1y <- NA
+for (i in min(aa$year):max(aa$year)){
+   qq$change1y[qq$year == i] <- aa$change[aa$year == i]
+   qq$change1y[qq$year == i+0.25] <- aa$change[aa$year == i]
+   qq$change1y[qq$year == i+0.50] <- aa$change[aa$year == i]
+   qq$change1y[qq$year == i+0.75] <- aa$change[aa$year == i]
+}
+gdp <- data.frame(qq$year, qq$change1y, qq$change, qq$change4q)
+colnames(gdp) <- c("year", "year_1", "qtrs_1", "qtrs_4")
+print(gdp)
+print(tail(gdp,40))
+X11()
+gdpm <- melt(gdp, id="year", value.name="GDP")
+gdpm <- gdpm[gdpm$year >= 2008,]
+gg <- ggplot(gdpm, aes(x=year, y=GDP, group=variable)) +
+   geom_line(aes(color = variable), size = 1, alpha = 0.7) +
+   geom_hline(yintercept = 3) +
+   geom_vline(xintercept = 2009) +
+   geom_vline(xintercept = 2017) +
+   scale_x_continuous(breaks = seq(2008,2017,2)) +
+   ggtitle("Real U.S. GDP Growth") +
+   labs(x = "Year", y = "Percent Annualized")
+print(gg)
